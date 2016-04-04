53d5140f8b75d2a81edc5a273607b17b4c25955e
kelvinmuchui <muchuikelvin423@gmail.com>
2016-04-04 07:55:39 +0300

    adding a blog post

--- app/views.py
+++ app/views.py
@@ -4,7 +4,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'muchui'}
+    user = {'nickname': 'muchui'} #fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
     return render_template('index.html',
                           title='home',
                           user=user)
