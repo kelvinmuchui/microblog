6247fe2bed1e6fc8b6c341ced065d895bc3fe17c
kelvinmuchui <muchuikelvin423@gmail.com>
2016-05-05 06:56:54 +0300

    added a html template

--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'muchui'}
-    return '''
-<html>
-<head>
-<body>
-<h1> Hello, ''' + user['nickname'] +'''</h1>
-</body>
-</html>
-'''
+    return render_template('index.html',
+                          title='home',
+                          user=user)
+    #return '''
+#<html>
+#<head>
+#<body>
+#<h1> Hello, ''' + user['nickname'] +'''</h1>
+#</body>
+#</html>
+#'''
